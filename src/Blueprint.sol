1f8b098f9b70a6643c84fa7d6cc734e850e7ab36
Charif Mews <charif@westaysafe.com>
2023-08-12 12:07:18 +0200

    Blueprint contract start

---
+++ src/Blueprint.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+// Import
+// Interfaces
+// Libraries
+// Contracts
+
+contract BluePrint {
+    // Variables
+
+    // Events
+
+    // Mods
+
+    // Structs, Arrays, Enums
+    // Constructor
+
+    // External functions
+
+    // Public functions
+
+    // Internal functions
+
+    // Private functions
+}
