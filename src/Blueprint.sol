a871100b88bb132a3af95e1640975a3e407c1182
Charif Mews <charif@westaysafe.com>
2023-08-12 13:17:54 +0200

    Add deposit/withdrawRequest/withdraw functions

--- src/Blueprint.sol
+++ src/Blueprint.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+error NoValueSent();
+error NotEnoughBalance();
+error WithdrawLimitReached();
+error PendingWithdraw();
+error WithdrawNotUnlockedYet();
+
 contract Blueprint {
     // Variables
     mapping(address => uint256) private balances;
@@ -8,7 +14,8 @@ contract Blueprint {
     mapping(address => uint256) private blockUnlock;
 
     // Events
-    event WithdrawRequest(address indexed _balanceOwner, uint256 _amount);
+    event Deposit(address indexed _balanceOwner, uint256 _amount);
+    event WithdrawRequest(address indexed _balanceOwner, uint256 _withdawLimit, uint256 _amount);
     event Withdraw(address indexed _balanceOwner, uint256 _amount);
     event Delay(address indexed _balanceOwner);
     event BlockAddress(address indexed _balanceOwner);
@@ -22,6 +29,29 @@ contract Blueprint {
     // Constructor
 
     // External functions
+    function deposit() external payable {
+        if (!(msg.value > 0)) revert NoValueSent();
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdrawRequest(uint256 _amount) external {
+        if (balances[msg.sender] < _amount) revert NotEnoughBalance();
+        if (withdawLimit[msg.sender] + _amount > balances[msg.sender]) revert WithdrawLimitReached();
+        if (blockUnlock[msg.sender] > block.number) revert PendingWithdraw();
+        withdawLimit[msg.sender] += _amount;
+        // 21600 blocks = 12 hours delay to withdraw considering 2s per block
+        blockUnlock[msg.sender] = block.number + 21600;
+        emit WithdrawRequest(msg.sender, withdawLimit[msg.sender], _amount);
+    }
+
+    function withdraw() external {
+        if (blockUnlock[msg.sender] > block.number) revert WithdrawNotUnlockedYet();
+        uint256 amount = withdawLimit[msg.sender];
+        withdawLimit[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+        emit Withdraw(msg.sender, amount);
+    }
 
     // Public functions
 
