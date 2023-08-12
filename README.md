d5977525c5a483053731489280eec83dd713f4e6
Charif Mews <charif@westaysafe.com>
2023-08-12 12:38:26 +0200

    Blueprint variables/events

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Foundry
+## Blueprint Contract WeStaySafe
 
 **Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
 
--- src/Blueprint.sol
+++ src/Blueprint.sol
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
-// Import
-// Interfaces
-// Libraries
-// Contracts
-
-contract BluePrint {
+contract Blueprint {
     // Variables
+    mapping(address => uint256) private balances;
+    mapping(address => uint256) private withdawLimit;
+    mapping(address => uint256) private blockUnlock;
 
     // Events
+    event WithdrawRequest(address indexed _balanceOwner, uint256 _amount);
+    event Withdraw(address indexed _balanceOwner, uint256 _amount);
+    event Delay(address indexed _balanceOwner);
+    event BlockAddress(address indexed _balanceOwner);
+    event UnlockAddress(address indexed _balanceOwner);
+    event CircuitBreak(uint256 _totalAmount);
+    event ResetCircuitBreak(uint256 _totalAmount);
 
     // Mods
 
